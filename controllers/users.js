c7ab148e6c88e2a2da810b940b4d9f939a7d82be
Andi Rustianto <anditodev46@gmail.com>
2022-03-24 23:29:12 +0700

    done

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,10 @@
 import { v4 as uuidv4 } from "uuid";
+import express from "express";
 uuidv4();
-export const getUser = (req, res) => {
+
+let users = [];
+
+export const getUsers = (req, res) => {
   res.send(users);
 };
 
@@ -23,6 +27,36 @@ export const getUserByID = (req, res) => {
     status: foundUser ? 200 : 404,
     message: foundUser ? `Get user with id ${id} succeed` : `No data available`,
     data: foundUser ? foundUser : null,
+  };
+
+  res.send(messageResponse);
+};
+
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+
+  const messageResponse = {
+    status: users ? 200 : 404,
+    message: users ? `Delete user with id ${id} succeed` : `No data available`,
+    data: users ? users : null,
+  };
+
+  res.send(messageResponse);
+};
+
+export const updateUser = (req, res) => {
+  const { id } = req.params;
+  const user = users.find((user) => user.id === id);
+  const { firstName, lastName, age } = req.body;
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
+
+  const messageResponse = {
+    status: 200,
+    message: `Updated user by id ${id} succeed`,
+    data: users ? users : null,
   };
 
   res.send(messageResponse);
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,17 @@
 import express from "express";
 
-import { createUser, getUser, getUserByID } from "../controllers/users";
+import {
+  createUser,
+  getUsers,
+  getUserByID,
+  deleteUser,
+  updateUser,
+} from "../controllers/users.js";
 
 const router = express.Router();
 
-let users = [];
-
 // all routes in here are starting with /users
-router.get("/", getUser);
+router.get("/", getUsers);
 
 // post or add user
 router.post("/", createUser);
@@ -16,34 +20,8 @@ router.post("/", createUser);
 router.get("/:id", getUserByID);
 
 //delete user by id
-router.delete("/:id", (req, res) => {
-  const { id } = req.params;
-  users = users.filter((user) => user.id !== id);
-
-  const messageResponse = {
-    status: users ? 200 : 404,
-    message: users ? `Delete user with id ${id} succeed` : `No data available`,
-    data: users ? users : null,
-  };
-
-  res.send(messageResponse);
-});
+router.delete("/:id", deleteUser);
 // update user by id
-router.patch("/:id", (req, res) => {
-  const { id } = req.params;
-  const user = users.find((user) => user.id === id);
-  const { firstName, lastName, age } = req.body;
-  if (firstName) user.firstName = firstName;
-  if (lastName) user.lastName = lastName;
-  if (age) user.age = age;
-
-  const messageResponse = {
-    status: 200,
-    message: `Updated user by id ${id} succeed`,
-    data: users ? users : null,
-  };
-
-  res.send(messageResponse);
-});
+router.patch("/:id", updateUser);
 
 export default router;
